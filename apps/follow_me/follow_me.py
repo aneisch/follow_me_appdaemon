d1cf84748cef63d754d2d086a2eb368623f5dcc8
Andrew <andrewneisch@gmail.com>
2021-12-06 11:03:52 -0600

    Update follow_me.py
    Add delay option

--- apps/follow_me/follow_me.py
+++ apps/follow_me/follow_me.py
@@ -7,21 +7,51 @@ class Follow(hass.Hass):
         for entity in self.split_device_list(self.args["leader"]):
             self.listen_state(self.state_change, entity)
 
+        # Defaults
+        self.delay = 0
+        self.invert = False
+        self.follow_on = True
+        self.follow_off = True
+
+        if "delay" in self.args:
+            self.delay = int(self.args["delay"])
+
+        if "invert" in self.args and self.args["invert"] == True:
+            self.invert = True
+
+        if "follow_on" in self.args:
+            self.follow_on = self.args["follow_on"]
+
+        if "follow_off" in self.args:
+            self.follow_off = self.args["follow_off"]
+
+    def turn_on_handler(self, kwargs):
+        self.turn_on(kwargs["entity_id"])
+
+    def turn_off_handler(self, kwargs):
+        self.turn_off(kwargs["entity_id"])
+
     def state_change(self, entity, attribute, old, new, kwargs):
         if new == "on" and old != "unavailable" and old != "Unavailable":
+            if not self.follow_on:
+                return
+
             for device in self.split_device_list(self.args["follower"]):
-                if "invert" in self.args:
-                    self.log("Turning off " + device)
-                    self.turn_off(device)
+                if self.invert:
+                    self.log(f"Turning off {device} in {self.delay} seconds")
+                    self.run_in(self.turn_off(device), self.delay)
                 else:
-                    self.log("Turning on " + device)
-                    self.turn_on(device)
+                    self.log(f"Turning on {device} in {self.delay} seconds")
+                    self.run_in(self.turn_on_handler, self.delay, entity_id=device)
 
         elif new == "off" and old != "unavailable" and old != "Unavailable":
+            if not self.follow_off:
+                return
+
             for device in self.split_device_list(self.args["follower"]):
-                if "invert" in self.args:
-                    self.log("Turning on " + device)
-                    self.turn_on(device)
+                if self.invert:
+                    self.log(f"Turning on {device} in {self.delay} seconds")
+                    self.run_in(self.turn_on(device), self.delay)
                 else:
-                    self.log("Turning off " + device)
-                    self.turn_off(device)
+                    self.log(f"Turning off {device} in {self.delay} seconds")
+                    self.run_in(self.turn_off_handler, self.delay, entity_id=device)
